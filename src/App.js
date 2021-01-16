a47ff875e85cdf89dfa7cd67040e71d4ff2503a6
Damilare Oyedeji <fuglydami@gmail.com>
2021-01-16 20:31:41 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css'
 import content from './data.js'
 // import { useState } from 'react'
-import Footer from "./Footer"
+import Footer from './Footer'
 // import { FaBeer } from 'react-icons/fa'
 
 function App() {
@@ -19,24 +19,18 @@ function App() {
         <section className="mainContent">
           <h3>Top questions</h3>
           <div className="grid-container">
-            {content.map((data, index) => {
-              const {
-                button,
-                title,
-                titleOptions1,
-                titleOptions2,
-                titleOptions3,
-              } = data
+            {content.map((data) => {
+              const { id, button, title, titleOptions } = data
               return (
-                <div key={index}>
+                <div key={id}>
                   <div className="grid1">
                     <p className="p-button">{button}</p>
                   </div>
                   <div className="title-container">
                     <h2 className="h2-title">{title}</h2>
-                    <p className="h2-titleOptions">{titleOptions1}</p>
-                    <p className="h2-titleOptions">{titleOptions2}</p>
-                    <p className="h2-titleOptions">{titleOptions3}</p>
+                    {titleOptions.map((titleOption) => {
+                      return <p className="h2-titleOptions">{titleOption}</p>
+                    })}
                   </div>
                 </div>
               )
--- src/data.js
+++ src/data.js
@@ -3,33 +3,25 @@ const content = [
     id: 1,
     button: 'About Actions',
     title: 'documentation',
-    titleOptions1: 'Help',
-    titleOptions2: 'Enterprise',
-    titleOptions3: 'Desktop',
+    titleOptions: ['Help', 'Enterprise', 'Desktop'],
   },
   {
     id: 2,
     button: 'Managing packages',
     title: 'community',
-    titleOptions1: 'Education',
-    titleOptions2: 'Atom',
-    titleOptions3: 'Forum',
+    titleOptions: ['Education', 'Atom', 'Forum'],
   },
   {
     id: 3,
     button: 'Authentication',
     title: 'news',
-    titleOptions1: ' Blog',
-    titleOptions2: 'Changelog',
-    titleOptions3: 'Status',
+    titleOptions: [' Blog', 'Changelog', 'Status'],
   },
   {
     id: 4,
     button: 'Notifications',
     title: 'learning',
-    titleOptions1: ' Lab',
-    titleOptions2: 'Guides',
-    titleOptions3: 'Resources',
+    titleOptions: ['Lab', 'Guides','Resources']
   },
 ]
 export default content
